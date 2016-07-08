a7e30370967f84e585dc2db91a8ed9cfd65a93f9
Annie Bae <bae.annie@gmail.com>
2016-07-08 15:32:11 -0400

    HelloWorld coponent

--- app/index.js
+++ app/index.js
@@ -1,2 +1,18 @@
-var app = document.getElementById('app');
-app.innerHTML = "Hello!"
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// define a component
+var HelloWorld = React.createClass({
+	render: function() {
+		return(
+			<div>Hello World</div>
+		)
+	}
+});
+
+
+ReactDOM.render(
+	// invoke a component
+	<HelloWorld />,
+	document.getElementById('app')
+);
